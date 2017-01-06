8fb1bddf3f44f80cc96a777eaa4f44f9418c1353
Claire Wang <clairewang96@gmail.com>
2017-01-06 21:43:25 +0800

    cleaned up unused images

--- send-mail.php
+++ send-mail.php
@@ -15,7 +15,7 @@ $message=$_POST['message'];
     $body .= "Message: " . $message . "\n"; 
 
     //replace with your email
-    mail("test@email.com","New email",$body); 
+    mail("ywang8@brynmawr.edu","Email from BM Engineers &amp; Makers Website",$body); 
 
   
 ?>
